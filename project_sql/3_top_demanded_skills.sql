b12bb34c4247e37552879b927754bdb2e10f1941
Nick Feichtel <nfeichtel@gmail.com>
2024-08-21 16:06:37 -0700

    demanded skills final

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -7,7 +7,15 @@ Focus on all job postings.
 - Why? Retrieves the top 5 skills with the highest demand in the job market, 
 providing insights into the most valuable skills for job seekers
 */
-SELECT *
+SELECT 
+    skills,
+    COUNT(skd.skill_id) AS demand_count
 FROM job_postings_fact jpf
-
-LIMIT 10
+JOIN skills_job_dim skd
+    ON jpf.job_id = skd.job_id
+JOIN skills_dim sd
+    ON skd.skill_id = sd.skill_id
+WHERE job_title_short = 'Data Analyst'
+GROUP BY skills
+ORDER BY demand_count DESC
+LIMIT 5
